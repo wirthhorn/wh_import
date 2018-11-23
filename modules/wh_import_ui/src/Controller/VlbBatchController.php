b06b41c7bd345d21af63858ccc62903766554667
Bettina Händel <bettina@atelier.wirth-horn.de>
2018-11-23 13:38:45 +0100

    change error handling

--- modules/wh_import_ui/src/Controller/VlbBatchController.php
+++ modules/wh_import_ui/src/Controller/VlbBatchController.php
@@ -14,27 +14,31 @@ class VlbBatchController {
    *   Return markup string.
    */
   public function importBooks() {
+    try {
 
-    $batch = array(
-      'title' => t('importing books'),
-      'init_message'     => t('Start importing books'),
-      'progress_message' => t('Processed @current out of @total.'),
-      'error_message'    => t('An error occurred during processing'),
-      'finished' => 'demo_batch_complete_callback',
-      //'file' => drupal_get_path('module', 'demo_batch') . '/demo_batch.mybatch.inc',
-    );
-    //loop over nids
-    $eans = $this->getEans();
-    $limit = 0;
-    foreach ($eans as $ean) {
-      // $limit++;
-      // if($limit > 20){
-      //   break;
-      // }
-      $batch['operations'][] = ['\Drupal\wh_import_ui\Controller\VlbController::importBook',[$ean]];
+      $batch = array(
+        'title' => t('importing books'),
+        'init_message'     => t('Start importing books'),
+        'progress_message' => t('Processed @current out of @total.'),
+        'error_message'    => t('An error occurred during processing'),
+        'finished' => 'batchFinished',
+        //'file' => drupal_get_path('module', 'demo_batch') . '/demo_batch.mybatch.inc',
+      );
+      //loop over nids
+      $eans = $this->getEans();
+      $limit = 0;
+      foreach ($eans as $ean) {
+        // $limit++;
+        // if($limit > 20){
+        //   break;
+        // }
+        $batch['operations'][] = ['\Drupal\wh_import_ui\Controller\VlbController::importBook',[$ean]];
+      }
+      batch_set($batch);
+      return batch_process('user');
+    }catch (\Exception $e) {
+      \Drupal::logger('wh_import_batch')->error($e->getMessage());
     }
-    batch_set($batch);
-    return batch_process('user');
   }
 
   private function getEans(){
@@ -44,4 +48,21 @@ class VlbBatchController {
     return $eans;
   }
 
+  public function batchFinished($success, $results, $operations) {
+    \Drupal::logger('wh_import_batch')->notice('$success: '.$success.' - $results: '.$results.' - $operations: '.$operations);
+    if ($success) {
+     drupal_set_message(t("The contents are successfully imported from VLB source."));
+     \Drupal::logger('wh_import_batch')->notice("Batch Import successfull!");
+    }
+    else {
+      $error_operation = reset($operations);
+      drupal_set_message(t('An error occurred while processing @operation with arguments : @args', array('@operation' => $error_operation[0], '@args' => print_r($error_operation[0], TRUE))));
+      \Drupal::logger('wh_import_batch')->error("Batch Import failed!");
+    }
+    dpm('$success: '.$success);
+    dpm('$results: '.$results);
+    dpm('$operations: '.$operations);
+
+
+  }
 }
